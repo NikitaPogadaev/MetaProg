ec51eae694eb8d4c568d9322de4ef9c8e2bce9e1
NikitaPogadaev <pognick@mail.ru>
2023-11-10 03:30:40 +0300

    prom

--- task4/PolymorphicMapper.hpp
+++ task4/PolymorphicMapper.hpp
@@ -2,11 +2,23 @@
 
 #include <optional>
 
+#include <concepts>
 
 template <class From, auto target>
-struct Mapping;
+struct Mapping{
+  using getFrom = From;
+  static constexpr auto getTarget{target};
+};
 
-template <class Base, class Target, class... Mappings>
+template<class Mapping>
+concept RealMapping = requires(){Mapping::getFrom; Mapping::getTarget;};
+
+template<class Mapping, class Base, class Target>
+concept CorrectMapping = RealMapping<Mapping> && std::derived_from<Mapping::getFrom, Base> && std::same_as(Target, decltype(Mapping::getTarget));
+
+template <class Base, class Target, CorrectMapping<Base, Target>... Mappings> // requires(CorrectMapping<Mappings, Base, Target> ... )
 struct PolymorphicMapper {
+
+  template<TypeList TL>
   static std::optional<Target> map(const Base& object);
 };
