4674b4d0fae345ee8bd9d21a8d47086cbac5e34e
NikitaPogadaev <pognick@mail.ru>
2023-11-11 05:04:07 +0300

    first part

--- task4/FixedString.hpp
+++ task4/FixedString.hpp
@@ -1,12 +1,31 @@
 #pragma once
+#include<string_view>
 
-
-template<size_t max_length>
+template<std::size_t max_length>
 struct FixedString {
-  FixedString(const char* string, size_t length);
-  operator std::string_view() const;
+  constexpr FixedString(const char* string, std::size_t length): data_(), sz_(length){
+    for(std::size_t i = 0; i < length; ++i){
+        data_[i] = *(string + i);
+    }
+  }
+  constexpr operator std::string_view() const{
+    if (sz_ == 0 || max_length == 0){
+        return std::string_view();
+    }
+    else{
+        return std::string_view(&(data_[0]), sz_);
+    }
+  }
 
-  // std::string impl; ???
+
+
+// private:
+  char data_[max_length];
+  std::size_t sz_{0};
 };
 
 // operator ""_cstr ?
+
+constexpr FixedString<256> operator ""_cstr(const char* str, std::size_t ln){
+    return FixedString<256>(str, ln);
+}
--- task4/PolymorphicMapper.hpp
+++ task4/PolymorphicMapper.hpp
@@ -3,22 +3,44 @@
 #include <optional>
 
 #include <concepts>
+#include "../task3/type_lists.hpp"
 
 template <class From, auto target>
 struct Mapping{
   using getFrom = From;
+  using getTargetType = decltype(target);
   static constexpr auto getTarget{target};
 };
 
 template<class Mapping>
-concept RealMapping = requires(){Mapping::getFrom; Mapping::getTarget;};
+concept RealMapping = requires(){typename Mapping::getFrom; Mapping::getTarget;};
 
 template<class Mapping, class Base, class Target>
-concept CorrectMapping = RealMapping<Mapping> && std::derived_from<Mapping::getFrom, Base> && std::same_as(Target, decltype(Mapping::getTarget));
+concept CorrectMapping = RealMapping<Mapping> && 
+std::derived_from<typename Mapping::getFrom, Base> && 
+std::same_as<Target, typename Mapping::getTargetType>;
 
 template <class Base, class Target, CorrectMapping<Base, Target>... Mappings> // requires(CorrectMapping<Mappings, Base, Target> ... )
 struct PolymorphicMapper {
 
-  template<TypeList TL>
-  static std::optional<Target> map(const Base& object);
+  template<type_lists::TypeSequence TL>
+  static std::optional<Target> mapHelper(const Base& object){
+    if(const typename TL::Head::getFrom * ptr = dynamic_cast<const typename TL::Head::getFrom *>(&object); ptr != nullptr){
+      return TL::Head::getTarget;
+    }
+    else{
+      return mapHelper<typename TL::Tail>(object);
+    }
+  }
+  template<type_lists::Empty TL>
+  static std::optional<Target> mapHelper(const Base&){
+    return std::nullopt;
+  }
+
+  static std::optional<Target> map(const Base& object){
+    return mapHelper<type_lists::FromTuple<type_tuples::TTuple<Mappings ...>>>(object);
+  }
+
+
+
 };
